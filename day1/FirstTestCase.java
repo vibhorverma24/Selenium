90d4fcc4e593e1154a8b668bd2ae62d562594efb
Vibhor <vibhorverma8790@gmail.com>
2026-02-09 22:27:02 +0530

    Day 1 Selenium

---
+++ day1/FirstTestCase.java
@@ -0,0 +1,32 @@
+package day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/*
+ Test Case
+ 1) Launch Browser(chrome)
+ 2) Open Url https://demo.opencart.com/
+ 3) validate title should be "Your Store"
+ 4) Close browser
+*/
+public class FirstTestCase{
+	public static void main(String[] args) throws InterruptedException {
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--incognito");
+		WebDriver driver = new ChromeDriver(option);
+		Thread.sleep(5000);
+		driver.get("https://demo.opencart.com/");
+		Thread.sleep(5000);
+		driver.manage().window().maximize();
+		
+		String title = driver.getTitle();
+		if(title.equals("Your Store")) {
+			System.out.println("Test Case Passed");
+		}
+		else {
+			System.out.println("Test Case Failed");
+		}
+	}
+}
